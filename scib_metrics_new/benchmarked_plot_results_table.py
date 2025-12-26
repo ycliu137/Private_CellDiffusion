9f9befac732e1428552b3bbefa8fe5fe83eef2a8
Yuchen Liu <ycliu137@scc1.bu.edu>
2025-12-26 16:22:33 -0500

    Add scib-metrics

---
+++ scib_metrics_new/benchmarked_plot_results_table.py
@@ -0,0 +1,133 @@
+import os
+import warnings
+from dataclasses import asdict, dataclass
+from enum import Enum
+from functools import partial
+from typing import Any, Callable, Optional, Union
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scanpy as sc
+from anndata import AnnData
+from plottable import ColumnDefinition, Table
+from plottable.cmap import normed_cmap
+from plottable.plots import bar
+from sklearn.preprocessing import MinMaxScaler
+from tqdm import tqdm
+
+import scib_metrics
+from scib_metrics.nearest_neighbors import NeighborsResults, pynndescent
+
+_LABELS = "labels"
+_BATCH = "batch"
+_X_PRE = "X_pre"
+_METRIC_TYPE = "Metric Type"
+_AGGREGATE_SCORE = "Aggregate score"
+
+# Mapping of metric fn names to clean DataFrame column names
+metric_name_cleaner = {
+    "silhouette_label": "Silhouette label",
+    "silhouette_batch": "Silhouette batch",
+    "isolated_labels": "Isolated labels",
+    "nmi_ari_cluster_labels_leiden_nmi": "Leiden NMI",
+    "nmi_ari_cluster_labels_leiden_ari": "Leiden ARI",
+    "nmi_ari_cluster_labels_kmeans_nmi": "KMeans NMI",
+    "nmi_ari_cluster_labels_kmeans_ari": "KMeans ARI",
+    "clisi_knn": "cLISI",
+    "ilisi_knn": "iLISI",
+    "kbet_per_label": "KBET",
+    "graph_connectivity": "Graph connectivity",
+    "pcr_comparison": "PCR comparison",
+}
+
+def plot_results_table_new(
+        self, min_max_scale: bool = True, show: bool = True, save_fig: Optional[str] = None
+) -> Table:
+    """Plot the benchmarking results.
+
+    Parameters
+    ----------
+    min_max_scale
+        Whether to min max scale the results.
+    show
+        Whether to show the plot.
+    save_dir
+        The directory to save the plot to. If `None`, the plot is not saved.
+    """
+    num_embeds = len(self._embedding_obsm_keys)
+    cmap_fn = lambda col_data: normed_cmap(col_data, cmap=matplotlib.cm.PRGn, num_stds=2.5)
+    df = self.get_results(min_max_scale=min_max_scale)
+    # Do not want to plot what kind of metric it is
+    plot_df = df.drop(_METRIC_TYPE, axis=0)
+    # Sort by total score
+    plot_df = plot_df.sort_values(by="Total", ascending=False).astype(np.float64)
+    plot_df["Method"] = plot_df.index
+
+    # Split columns by metric type, using df as it doesn't have the new method col
+    score_cols = df.columns[df.loc[_METRIC_TYPE] == _AGGREGATE_SCORE]
+    other_cols = df.columns[df.loc[_METRIC_TYPE] != _AGGREGATE_SCORE]
+    column_definitions = [
+        ColumnDefinition("Method", width=1.5, textprops={"ha": "left", "weight": "bold"}),
+    ]
+    # Circles for the metric values
+    column_definitions += [
+        ColumnDefinition(
+            col,
+            title=col.replace(" ", "\n", 1),
+            width=1,
+            textprops={
+                "ha": "center",
+                "bbox": {"boxstyle": "circle", "pad": 0.25},
+            },
+            cmap=cmap_fn(plot_df[col]),
+            group=df.loc[_METRIC_TYPE, col],
+            formatter="{:.2f}",
+        )
+        for i, col in enumerate(other_cols)
+    ]
+    # Bars for the aggregate scores
+    column_definitions += [
+        ColumnDefinition(
+            col,
+            width=1,
+            title=col.replace(" ", "\n", 1),
+            plot_fn=bar,
+            plot_kw={
+                "cmap": matplotlib.cm.YlGnBu,
+                "plot_bg_bar": False,
+                "annotate": True,
+                "height": 0.9,
+                "formatter": "{:.2f}",
+            },
+            group=df.loc[_METRIC_TYPE, col],
+            border="left" if i == 0 else None,
+        )
+        for i, col in enumerate(score_cols)
+    ]
+    # Allow to manipulate text post-hoc (in illustrator)
+    with matplotlib.rc_context({"svg.fonttype": "none"}):
+        fig, ax = plt.subplots(figsize=(len(df.columns) * 1.25, 3 + 0.3 * num_embeds))
+        tab = Table(
+            plot_df,
+            cell_kw={
+                "linewidth": 0,
+                "edgecolor": "k",
+            },
+            column_definitions=column_definitions,
+            ax=ax,
+            row_dividers=True,
+            footer_divider=True,
+            textprops={"fontsize": 10, "ha": "center"},
+            row_divider_kw={"linewidth": 1, "linestyle": (0, (1, 5))},
+            col_label_divider_kw={"linewidth": 1, "linestyle": "-"},
+            column_border_kw={"linewidth": 1, "linestyle": "-"},
+            index_col="Method",
+        ).autoset_fontcolors(colnames=plot_df.columns)
+    if show:
+        plt.show()
+    if save_fig is not None:
+        fig.savefig(save_fig, facecolor=ax.get_facecolor(), dpi=300)
+
+    return fig
